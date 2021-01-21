7c8f5a9602d3a506d1654403888f3b850b3675e0
Charles Ivia <iviacharles@gmail.com>
2021-01-21 21:45:37 +0300

    Plotting a 3d scatter plot in seaborn

--- scatterplots_sns_adv.py
+++ scatterplots_sns_adv.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from pandas.io.formats import style
 import seaborn as sns
 import pandas as pd
 
@@ -18,4 +19,22 @@ grid = sns.FacetGrid(df, col="Region", hue="Region", col_wrap=5)
 grid.map(sns.scatterplot, "Economy (GDP per Capita)", "Health (Life Expectancy)")
 
 grid.add_legend()
+
+# Plotting a 3D scatter plot in seaborn
+
+sns.set(style="darkgrid")
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+
+x = df["Happiness Score"]
+y = df["Economy (GDP per Capita)"]
+z = df["Health (Life Expectancy)"]
+
+ax.set_xlabel("Happiness")
+ax.set_ylabel("Economy")
+ax.set_zlabel("Health")
+
+ax.scatter(x, y, z)
+
 plt.show()
